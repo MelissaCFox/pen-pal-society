d7fac03ca277f17693ed2caf14db468df9121736
MelissaCFox <melissa.caitlin.fox@gmail.com>
2021-11-03 11:13:29 -0500

    adjusted styling

--- src/scripts/PenPalSociety.js
+++ src/scripts/PenPalSociety.js
@@ -5,6 +5,7 @@ import { SentLetters } from "./SentLetters.js"
 export const PenPalSociety = () => {
     return `
         <h1>Pen Pal Society</h1>
+        <h2>Write A Letter</h2>
         <section class="letterForm">
             ${LetterForm()}
         </section>
